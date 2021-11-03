675416e4f5f57493e37d26aad865e477c82d8442
carlosandf <carlos.andres.ff2912@gmail.com>
2021-11-03 17:09:42 -0500

    hice cambios

--- Promedio/promedio.js
+++ Promedio/promedio.js
@@ -1,18 +1,32 @@
+const lista = [];
+
+function pushArray() {
+  const input = document.getElementById("inpuPromedio");
+  const value = Number(input.value);
+  const elements = lista;
+  
+  elements.push(value);
+  return console.log(elements);
+
+}
+
+
+console.log(lista);
 
 function calcularMediaAritmetica(lista){
-/* let sumaLista = 0;
-   for(let i = 0; i < lista.length; i++)
-   {
-     sumaLista = sumaLista + lista[i];
-   }
-*/
+
    const sumaLista = lista.reduce(
        function (valorAcumulado = 0, nuevoElemento) {
          return valorAcumulado + nuevoElemento;
        }
     );
- 
     const promedioLista = sumaLista / lista.length;
     
     return promedioLista;
 }
+
+// function calcular(lista) {
+//   const elements = lista;
+//   const resutlt = calcularMediaAritmetica(elements);
+//   return console.log(resutlt);
+// }
